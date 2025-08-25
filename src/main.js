5d23552af796f758ec4456f0c8ac75a1090147fc
Uxaxaxa <lytonin.v@gmail.com>
2025-08-25 15:03:35 +0500

    add modal

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
 import './styles/style.scss'
 
+
+const modal = document.getElementById("leadModal");
+const openBtns = document.querySelectorAll(".btn--primary");
+const closeBtn = modal.querySelector(".modal__close");
+const overlay = modal.querySelector(".modal__overlay");
+
+// открыть на любую кнопку с классом .btn--primary
+openBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        modal.classList.add("active");
+    });
+});
+
+// закрыть
+closeBtn.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+overlay.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
